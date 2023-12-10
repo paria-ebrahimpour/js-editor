5b8d7e5bc279f8fd60f6b09db6221b856dfa891d
paria.ebrahimpour1 <paria.ebrahimpour1@gamil.com>
2023-12-10 15:41:21 +0330

    feat: some cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [code, setCode] = useState<undefined | string>();
+  const [transpiledCode, setTranspiledCode] = useState();
+
+  const onSubmit = () => {
+    console.log(code);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      this is code js editor that you can import react, axios or ... in it and
+      use and test it
+      <br />
+      <textarea
+        value={code}
+        onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+          setCode(e?.target?.value)
+        }
+      ></textarea>
+      <div>
+        <button onClick={onSubmit}>Submit</button>
+      </div>
+      <pre>{transpiledCode}</pre>
     </div>
   );
 }
