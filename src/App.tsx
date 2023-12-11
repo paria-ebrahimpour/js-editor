50e20c58c7e1d8fc9c57cec2788450773fe7f55a
Paria Ebrahimpour <paria.ebrahimpour1@gmail.com>
2023-12-11 22:19:44 +0330

    feat: add plugin for esbuild

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 function App() {
-  const [code, setCode] = useState<undefined | string>();
-  const [transpiledCode, setTranspiledCode] = useState();
+  const ref = useRef<any>();
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
 
-  const onSubmit = () => {
-    console.log(code);
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
   };
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onSubmit = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
+    });
+
+    setCode(result.outputFiles[0].text);
+  };
+
   return (
     <div>
       this is code js editor that you can import react, axios or ... in it and
       use and test it
       <br />
       <textarea
-        value={code}
+        value={input}
         onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
-          setCode(e?.target?.value)
+          setInput(e?.target?.value)
         }
       ></textarea>
       <div>
         <button onClick={onSubmit}>Submit</button>
       </div>
-      <pre>{transpiledCode}</pre>
+      <pre>{code}</pre>
     </div>
   );
 }
