3b9061858fd36d66c99100f288eb30688b3f0b43
Paria Ebrahimpour <paria.ebrahimpour1@gmail.com>
2024-01-07 19:20:59 +0330

    fix: extract some codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,29 @@
-import React, { useEffect, useRef, useState } from "react";
-import * as esbuild from "esbuild-wasm";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
+import { useState } from "react";
 import CodeEditor from "./components/code-editor";
+import Preview from "./components/preview";
+import bundle from "./bundler";
 
 function App() {
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
+  const [code, setCode] = useState("");
   const [input, setInput] = useState("");
 
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: "/esbuild.wasm",
-    });
+  const onClick = async () => {
+    const output = await bundle(input);
+    setCode(output);
   };
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const onSubmit = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    iframe.current.srcdoc = html;
-
-    const result = await ref.current.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
-      },
-    });
-
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
-  };
-
-  const html = `
-  <html>
-    <head></head>
-    <body>
-      <div id="root"></div>
-      <script>
-        window.addEventListener('message', (event) => {
-          try {
-            eval(event.data);
-          } catch (err) {
-            const root = document.querySelector('#root');
-            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-            console.error(err);
-          }
-        }, false);
-      </script>
-    </body>
-  </html>
-`;
 
   return (
-    <div>
-      this is code js editor that you can import react, axios or ... in it and
-      use and test it
-      <br />
+    <>
       <CodeEditor
         initialValue="const a = 1;"
         onChange={(value) => setInput(value)}
       />
-      {/* <textarea
-        value={input}
-        onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
-          setInput(e?.target?.value)
-        }
-      ></textarea> */}
+
       <div>
-        <button onClick={onSubmit}>Submit</button>
+        <button onClick={onClick}>Submit</button>
       </div>
-      <iframe
-        title="preview"
-        ref={iframe}
-        // sandbox="allow-scripts"
-        srcDoc={html}
-      />
-    </div>
+      <Preview code={code} />
+    </>
   );
 }
 
